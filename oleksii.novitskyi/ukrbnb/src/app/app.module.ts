c31da09ce0d3fa5294439cef70f2a5891d0079a1
Oleksii Novitskyi <ONovitskyi@drbsystems.com>
2020-08-25 08:53:48 +0300

    Added all necessary components

--- oleksii.novitskyi/ukrbnb/src/app/app.module.ts
+++ oleksii.novitskyi/ukrbnb/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { HotelsComponent } from './hotels/hotels.component';
+import { ListComponent } from './list/list.component';
+import { WeatherComponent } from './weather/weather.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HotelsComponent,
+    ListComponent,
+    WeatherComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
